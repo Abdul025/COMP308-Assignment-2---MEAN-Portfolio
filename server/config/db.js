c0bb9d13605ca64a7c0ceab76b6857af99e133ad
Abdul025 <ghafor_n@live.com>
2019-03-29 18:24:00 -0400

    added mongodb URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
    //"URI" : "mongodb://localhost/test",
     "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
+   // "URI" : "mongodb://Abdul04:aG325039790@ds064748.mlab.com:64748/comp308-w2019-10b",
     "secret": "SomeSecret"
 }
